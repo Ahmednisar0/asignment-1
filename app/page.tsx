1bda801c7e76ad23661747bfd465d3c56fa765a1
Ahmednisar0 <159664876+Ahmednisar0@users.noreply.github.com>
2024-10-07 03:33:13 +0500

    hh

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function CountryList() {
   </div>
     <div className='text-center font-medium text-2xl py-2 '>
       <h1 className='text-center font-bold py-6  text-5xl bg-slate-400'>Country List</h1>
-      <ul  className="justify-items-center">
+      <ul  className="justify-items-end">
         {countries.map((country) => (
           <li key={country.name} className="inline-block px-4 py-2 mt-5 ml-5  bg-blue-500 text-white font-bold rounded hover:bg-blue-700 cursor-pointer">
             <Link href={`/country/${country.name.toLowerCase()}`}>
